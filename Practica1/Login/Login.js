46f9bcdfeeafb3639b99d80af4312d708041c684
Josue <wjosuep13@gmail.com>
2022-01-30 18:50:25 -0600

    Creación de server inicial servicio Login

---
+++ Practica1/Login/Login.js
@@ -0,0 +1,6 @@
+
+const users =[{username:"wjosuep13",password:'123',rol:1},{username:"edsilon",password:'123',rol:2},{username:"repartidor",password:'123',rol:3}]
+
+exports.login = function (req, res) {
+
+  };
--- Practica1/Login/server.js
+++ Practica1/Login/server.js
@@ -1,1 +1,29 @@
 
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const cors=require("cors");
+const corsOptions ={
+  origin: '*',
+  optionsSuccessStatus: 200 ,// For legacy browser support
+  methods: "GET, PUT, POST, DELETE, UPDATE"
+}
+// create express app
+const app = express();
+// Setup server port
+const port = process.env.PORT || 4000;
+app.use(cors(corsOptions));
+
+app.use(bodyParser.urlencoded({ extended: true }))
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+// define a root route
+app.get('/', (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+
+module.exports=app;
