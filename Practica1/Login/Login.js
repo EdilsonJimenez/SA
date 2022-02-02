9e9e9a72052694031dcab44a1542af18d4e553fb
Josue <wjosuep13@gmail.com>
2022-02-01 18:33:27 -0600

    Login with return token jwt

--- Practica1/Login/Login.js
+++ Practica1/Login/Login.js
@@ -1,6 +1,40 @@
 
-const users =[{username:"wjosuep13",password:'123',rol:1},{username:"edsilon",password:'123',rol:2},{username:"repartidor",password:'123',rol:3}]
+
+const jwt = require('jsonwebtoken');
+const users = new Map();
+users.set('wjosuep13', { password: '123', rol: 1 });
+users.set('edsilon', { password: '123', rol: 2 });
+users.set('repartidor', { password: '123', rol: 3 });
 
 exports.login = function (req, res) {
 
-  };
+  let user = req.body;
+  console.log(req.user);
+  let registeruser = users.get(user.username);
+  if (registeruser) {
+    if (registeruser.password === user.password) {
+      const token = jwt.sign({
+        name: user.username,
+        rol: registeruser.rol
+      }, process.env.TOKEN_SECRET)
+    
+      res.header('auth-token', token).json({
+        error: null,
+        data: { token }
+      })
+      res.json({
+        error: null,
+        data: 'exito bienvenido'
+      })
+
+    } else {
+
+      res.json({
+        error: 'Error al logearse',
+        data:null
+      })
+    }
+  }
+
+};
+
