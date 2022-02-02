76bb1f543d357c64d8131da1b9f2b4d6454e7f7a
Josue <wjosuep13@gmail.com>
2022-02-01 18:36:39 -0600

    removed res.json

--- Practica1/Login/Login.js
+++ Practica1/Login/Login.js
@@ -9,7 +9,6 @@ users.set('repartidor', { password: '123', rol: 3 });
 exports.login = function (req, res) {
 
   let user = req.body;
-  console.log(req.user);
   let registeruser = users.get(user.username);
   if (registeruser) {
     if (registeruser.password === user.password) {
@@ -17,21 +16,18 @@ exports.login = function (req, res) {
         name: user.username,
         rol: registeruser.rol
       }, process.env.TOKEN_SECRET)
-    
+
       res.header('auth-token', token).json({
         error: null,
         data: { token }
       })
-      res.json({
-        error: null,
-        data: 'exito bienvenido'
-      })
+
 
     } else {
 
       res.json({
         error: 'Error al logearse',
-        data:null
+        data: null
       })
     }
   }
