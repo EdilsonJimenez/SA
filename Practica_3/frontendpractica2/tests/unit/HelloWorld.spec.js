122a845c74beaf6002875021d0c0b429980361cf
eejimenez <ejimenez@cartfulsolutions.com>
2022-02-19 17:44:14 -0600

    Prueba fallida

--- Practica_3/frontendpractica2/tests/unit/HelloWorld.spec.js
+++ Practica_3/frontendpractica2/tests/unit/HelloWorld.spec.js
@@ -5,6 +5,6 @@ import HelloWorld from '@/components/HelloWorld.vue'
 describe('HelloWorld unit test', () => {
     test('renders the names', () => {
         const wrapper = mount(HelloWorld, {});
-        expect(wrapper.html()).toContain("Walter");
+        expect(wrapper.html()).toContain("Walter2");
     });
 });
