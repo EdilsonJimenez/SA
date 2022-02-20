368514742ba7d3d50dd297f61641514caafe928e
wjosuep13 <201504326@ingenieria.usac.edu.gt>
2022-02-19 21:35:16 -0600

    Update gh-pages-deploy.js

--- Practica_3/frontendpractica2/Scripts/gh-pages-deploy.js
+++ Practica_3/frontendpractica2/Scripts/gh-pages-deploy.js
@@ -12,7 +12,7 @@ const fs = require("fs");
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
-    await execa("git", ["push", "https://github.com/wjosuep13/SA.git", "main:gh-pages", "--force"]);
+    await execa("git", ["push", `https://wjosuep13:${process.env.pass}@github.com/wjosuep13/SA.git`, "main:gh-pages", "--force"]);
     console.log("Pusheadoxd..");
     await execa("git", ["checkout", "-f", "main"]);
     await execa("git", ["branch", "-D", "gh-pages"]);
