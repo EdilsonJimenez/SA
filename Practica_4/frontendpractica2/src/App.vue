2bb0afde4101efe3b32215f43f76798702e82f19
eejimenez <ejimenez@cartfulsolutions.com>
2022-02-24 18:54:27 -0600

    test coverage

---
+++ Practica_4/frontendpractica2/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img class="logo" alt="Vue logo" src="./assets/SA_Logo.png" />
+    <HelloWorld msg="Pareja 5  CICD" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+.logo {
+  width: 200px;
+  height: 200px;
+}
+#app {
+  background-color: rgb(46, 45, 45);
+
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #fdfdfd;
+  padding-block: 100px;
+  margin-top: 10%;
+  margin-left: 30%;
+  margin-right: 30%;
+  height: 100%;
+}
+</style>
---
+++ Practica_4/frontendpractica2/tests/unit/HelloWorld.spec.js
@@ -0,0 +1,10 @@
+import {mount} from '@vue/test-utils';
+import HelloWorld from '@/components/HelloWorld.vue'
+
+
+describe('HelloWorld unit test', () => {
+    test('renders the names', () => {
+        const wrapper = mount(HelloWorld, {});
+        expect(wrapper.html()).toContain("Walter");
+    });
+});
