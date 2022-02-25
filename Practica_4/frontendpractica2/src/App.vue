905fc7942cf18a4a20c5b71d474bf15a32124ced
wjosuep13 <201504326@ingenieria.usac.edu.gt>
2022-02-24 20:50:08 -0600

    test sq

--- Practica_4/frontendpractica2/src/App.vue
+++ Practica_4/frontendpractica2/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img class="logo" alt="Vue logo" src="./assets/SA_Logo.png" />
-    <HelloWorld msg="Pareja 5  Practica4 test sonarq cambio" />
+    <HelloWorld msg="Pareja 5  Practica4 test sonarq test2" />
   </div>
 </template>
 
