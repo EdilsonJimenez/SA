219292bca8d116a256b47a925da66dae0626f9f1
Josue <wjosuep13@gmail.com>
2022-03-04 17:56:01 -0600

    test ansible 1

--- Practica_4/frontendpractica2/src/App.vue
+++ Practica_4/frontendpractica2/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img class="logo" alt="Vue logo" src="./assets/SA_Logo.png" />
-    <HelloWorld msg="Pareja 5  Practica4 test sonarq test3" />
+    <HelloWorld msg="Pareja 5  Practica5 test" />
   </div>
 </template>
 
