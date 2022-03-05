f98f81c73e5edae967a749cac51c5f5fb8931f75
Josue <wjosuep13@gmail.com>
2022-03-04 18:15:07 -0600

    test3

--- Practica_4/frontendpractica2/src/App.vue
+++ Practica_4/frontendpractica2/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img class="logo" alt="Vue logo" src="./assets/SA_Logo.png" />
-    <HelloWorld msg="Pareja 5  Practica5 test2" />
+    <HelloWorld msg="Pareja 5  Practica5 test3" />
   </div>
 </template>
 
