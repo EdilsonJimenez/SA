bd00fed91fd79a9062a99376ec9ada3f81666f4c
eejimenez <ejimenez@cartfulsolutions.com>
2022-03-17 23:50:15 -0600

    removed variables

--- variables.tf
+++ variables.tf
@@ -1,26 +1,3 @@
-
-variable "project_id" {
-  description = "The project ID to deploy resources into"
-}
-
-variable "subnetwork_project" {
-  description = "The project ID where the desired subnetwork is provisioned"
-}
-
-variable "subnetwork" {
-  description = "The name of the subnetwork to deploy instances into"
-}
-
-variable "instance_name" {
-  description = "The desired name to assign to the deployed instance"
-  default     = "pareja5practica7"
-}
-
-variable "zone" {
-  description = "The GCP zone to deploy instances into"
-  type        = string
-}
-
 variable "client_email" {
   description = "Service account email address"
   type        = string
